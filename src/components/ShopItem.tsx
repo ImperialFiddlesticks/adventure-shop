896dd475d3e0a7113b5f8eb680a34e9abf3f7e17
ImperialFiddlesticks <bleckert.andrea@gmail.com>
2025-12-15 17:33:44 +0100

    Minimum viable

---
+++ src/components/ShopItem.tsx
@@ -0,0 +1,23 @@
+import type { IGood } from "../data/InitialData";
+
+interface IShopItem {
+  item: IGood;
+  onClick?: (item: IGood) => void;
+  buttonText?: string;
+}
+const ShopItem = ({ item, onClick, buttonText }: IShopItem) => {
+  return (
+    <div className="shopLine">
+      <img src={item.img} className="shopIcons" />
+      <p className="itemName">{item.name}</p>
+      <p className="itemPrice">Price: {item.price} Gold</p>
+
+      {onClick && buttonText && (
+        <button className="buyButton" onClick={() => onClick(item)}>
+          {buttonText}
+        </button>
+      )}
+    </div>
+  );
+};
+export default ShopItem;
---
+++ src/hooks/useShopInventory.ts
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import goods from "../data/InitialData";
+import type { IGood } from "../data/InitialData";
+
+export function useShopInventory() {
+  const [shopInventory, setShopInventory] = useState<IGood[]>(goods);
+  const [yourInventory, setYourInventory] = useState<IGood[]>([]);
+
+  function handleBuy(item: IGood) {
+    setShopInventory((prev) =>
+      prev.filter((shopItem) => shopItem.id !== item.id)
+    );
+    setYourInventory((prev) => [...prev, item]);
+  }
+  function handleSell(item: IGood) {
+    setYourInventory((prev) =>
+      prev.filter((invItem) => invItem.id !== item.id)
+    );
+    setShopInventory((prev) => [...prev, item]);
+  }
+
+  return {
+    shopInventory,
+    yourInventory,
+    handleBuy,
+    handleSell,
+  };
+}
